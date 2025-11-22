305254ed4e1799767e699fdcd27002ce5720adca
roldesch <rolandesch@proton.me>
2025-11-22 12:33:48 -0300

    test: Add test for unassigning teacher when no teacher is assigned

--- tests/domain/test_course.py
+++ tests/domain/test_course.py
@@ -1,6 +1,8 @@
 #test_course.py
 import pytest
 from exceptions.domain_exceptions import EnrollmentError, TeacherAssignmentError
+from tests.conftest import make_course
+
 
 def test_assigning_a_teacher_to_a_course_that_has_no_teacher_assigned_succeeds(make_course, make_teacher):
     # Arrange
@@ -41,3 +43,12 @@ def test_unassigning_a_teacher_from_a_course_that_already_has_a_teacher_assigned
     # Assert
     assert course.teacher is None
     assert course not in teacher.courses
+
+def test_unassigning_a_teacher_from_a_course_that_has_no_teacher_assigned_raises_teacherassignmenterror(make_course):
+    # Arrange
+    course = make_course()
+    assert  course.teacher is None  # precondition
+
+    # Act / Assert
+    with pytest.raises(TeacherAssignmentError):
+        course.unassign_teacher()
